0d60c8b69e089e402d402179c08af319d109f6d0
王尚 <fcode804@163.com>
2020-01-17 10:35:21 +0800

    初始化项目

---
+++ src/main/java/com/smkj/shiroAndJwt/ShiroAndJwtApplication.java
@@ -0,0 +1,13 @@
+package com.smkj.shiroAndJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroAndJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroAndJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smkj/shiroAndJwt/ShiroAndJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smkj.shiroAndJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroAndJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
